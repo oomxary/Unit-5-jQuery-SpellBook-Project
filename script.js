c28a709908ad3f0678a3fd83fd14b6d8908eec55
oomxary <44421231+oomxary@users.noreply.github.com>
2019-02-04 16:49:47 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#heartshake").click(function(){
+    $(".shake").slideToggle();
+});
+
+
+
+//Spell #2
+
+$("#likey").click(function(){
+    $(".likey").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("#yoy").click(function(){
+    $(".yoy").toggle();
+});
+
+
+
